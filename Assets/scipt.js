6633bd0ec699d133cc299a23f42872c36aa9d4a3
Jacques <jacques.scoffoni@gmail.com>
2023-04-27 19:12:06 -0400

    2-Questions and beginning of quizz function

--- Assets/scipt.js
+++ Assets/scipt.js
@@ -1,3 +1,7 @@
+let startButton = document.querySelector(".startButton");
+let timerDisplay = document.querySelector("#timer");
+let quizBox = document.querySelector("#popQuiz;")
+
 //Timer function
 function timer() {
     timerInterval = setInterval(function () {
@@ -7,5 +11,52 @@ function timer() {
             clearInterval(timerInterval);
             gameOver();
         }
+        if (currentTime === 0) {
+            clearInterval(timerInterval);
+            loseGame();
+        }
     }, 1000);
 }
+
+let questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript? ",
+        answers: ["js", "script", "javascript", "scripting"],
+        correctAnswer: ""
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: ["The <head> section", "The <body> section", "Both"],
+        correctAnswer: "Both",
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        answers: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js;>"],
+        correctAnswer: "<script href='xxx.js'>",
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: ["alert('Hello World')", "alertBox('Hello World')", "msg('Hello World')", "msgBox('Hello World')"],
+        correctAnswer: "alert('Hello World')",
+    }
+];
+
+function quiz(questions) {
+    let choices = [];    
+    let answers;
+    for (var i = 0; i < questions.length; i++) {
+        answers = [];
+
+        for (var j = 0; j < questions[i].answers.length; j++) {
+            let btn = document.createElement("answerButton");
+            let a = document.createTextNode((j+1) + questions[i].answers[j]);
+            btn.appendChild(a);
+            document.body.appendChild(btn);
+        }
+        choices.push(
+
+        )
+    }
+}
+
+startButton.addEventListener("click", quiz);
