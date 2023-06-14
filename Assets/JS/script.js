b8e6ec8d97f65db6bc3445625f65b43925f5a9b3
Jacques <jacques.scoffoni@gmail.com>
2023-06-14 01:14:08 -0400

    4-Quiz function code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,61 +1,102 @@
 let startButton = document.querySelector(".startButton");
-let timerDisplay = document.querySelector("#timer");
-let quizBox = document.querySelector("#popQuiz");
+let timerDisplay = document.getElementById("timer");
+let quizBox = document.getElementById("popQuiz");
 let timerCount = 60;
+let currentQuestionIndex = 0;
 
 //Timer function
 function timer() {
-    timerInterval = setInterval(function () {
-        timerCount--;
-        timerDisplay.textContent = "Time: " + timerCount;
-      
-        if (timerCount <= 0) {
-            clearInterval(timerInterval);
-            gameOver();
-        }
-    }, 1000);
+  timerInterval = setInterval(function () {
+    timerCount--;
+    timerDisplay.textContent = "Time: " + timerCount;
+
+    if (timerCount <= 0) {
+      clearInterval(timerInterval);
+      gameOver();
+    }
+  }, 1000);
+  quiz();
 }
 
 let questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript? ",
-        answers: ["<js>", "<script>", "<javascript>", "<scripting>"],
-        correctAnswer: "<script>"
-    },
-    {
-        question: "Where is the correct place to insert a JavaScript?",
-        answers: ["The <head> section", "The <body> section", "Both"],
-        correctAnswer: "Both"
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        answers: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js;>"],
-        correctAnswer: "<script href='xxx.js'>"
-    },
-    {
-        question: "How do you write 'Hello World' in an alert box?",
-        answers: ["alert('Hello World')", "alertBox('Hello World')", "msg('Hello World')", "msgBox('Hello World')"],
-        correctAnswer: "alert('Hello World')"
-    }
+  {
+    question: "Inside which HTML element do we put the JavaScript? ",
+    choices: ["<js>", "<script>", "<javascript>", "<scripting>"],
+    answer: "<script>",
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    choices: ["The <head> section", "The <body> section", "Both"],
+    answer: "Both",
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choices: [
+      "<script href='xxx.js'>",
+      "<script name='xxx.js'>",
+      "<script src='xxx.js;>",
+    ],
+    answer: "<script href='xxx.js'>",
+  },
+  {
+    question: "How do you write 'Hello World' in an alert box?",
+    choices: [
+      "alert('Hello World')",
+      "alertBox('Hello World')",
+      "msg('Hello World')",
+      "msgBox('Hello World')",
+    ],
+    answer: "alert('Hello World')",
+  },
 ];
 
-function quiz(questions) {    
-    let choices = [];    
-    let answers;
-    for (var i = 0; i < questions.length; i++) {
-        answers = [];
+function quiz() {  
+  let currentQuestion = questions[currentQuestionIndex];
 
-        for (var j = 0; j < questions[i].answers.length; j++) {
-            let btn = document.createElement("answerButton");
-            let a = document.createTextNode((j+1) + questions[i].answers[j]);
-            btn.appendChild(a);
-            document.body.appendChild(btn);
-        }
-        choices.push(
+  let questionElement = document.createElement("h2");
+  questionElement.id = "question";
+  questionElement.textContent = currentQuestion.question;
 
-        )
-    }
-    timer();
+  let choicesBox = document.createElement("div");
+  choicesBox.id = "choices";
+  currentQuestion.choices.forEach(function (choice) {
+    let choiceButton = document.createElement("button");
+    choiceButton.textContent = choice;
+    choiceButton.addEventListener("click", function () {
+      checkAnswer(choice, currentQuestion.answer);
+    });
+    choicesBox.appendChild(choiceButton);
+  });
+
+  quizBox.innerHTML = "";
+  quizBox.appendChild(questionElement);
+  quizBox.appendChild(choicesBox);
 }
 
-startButton.addEventListener("click", quiz);
+function checkAnswer(choice, answer) {
+  if (choice === answer) {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      quiz();
+    } else {
+      gameOver();
+    }
+  } else {
+    timerCount -= 10;
+    if (timerCount < 0) {
+      timerCount = 0;
+    }
+  }
+}
+
+function gameOver() {
+  clearInterval(timerInterval);
+
+  localStorage.setItem("score", timerCount);
+
+  // Redirect to results page
+  window.location.href = "./results.html";
+}
+
+startButton.addEventListener("click", timer);
