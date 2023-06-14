f978194633f23f497601e9cc0af0868496c22389
Jacques <jacques.scoffoni@gmail.com>
2023-06-14 19:43:05 -0400

    8-Results js, html and css

---
+++ Assets/JS/highscores.js
@@ -0,0 +1,28 @@
+let highScoresList = document.getElementById("highScoresList");
+
+// Retrieve high scores from localStorage or initialize an empty array
+let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+let currentScore = localStorage.getItem("score");
+let initials = localStorage.getItem("initials");
+
+// Add the current score and initials to the high scores array
+if (currentScore && initials) {
+  let newScore = {
+    initials: initials,
+    score: currentScore
+  };
+  highScores.push(newScore);
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+// Sort high scores in descending order
+highScores.sort(function(a, b) {
+  return b.score - a.score;
+});
+
+// Display high scores
+highScores.forEach(function(scoreEntry) {
+  let scoreItem = document.createElement("li");  
+  scoreItem.textContent = scoreEntry.initials + " - " + scoreEntry.score;
+  highScoresList.appendChild(scoreItem);
+});
